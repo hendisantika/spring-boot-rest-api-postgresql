8a5a7afd13042cb0917901e2c411669edb505a2c
Hendi Santika <hendisantika@gmail.com>
2019-01-18 22:06:09 +0700

    #3 - Add Book Entity

---
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/entity/Book.java
@@ -0,0 +1,77 @@
+package com.hendisantika.springbootrestapipostgresql.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-18
+ * Time: 22:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String description;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tag")
+    @Column(name = "Value")
+    private List<String> tags = new ArrayList<>();
+
+    public Book(String name, String description, List<String> tags) {
+        this.name = name;
+        this.description = description;
+        this.tags = tags;
+    }
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
+}
