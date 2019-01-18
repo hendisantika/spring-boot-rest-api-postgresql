185f2a969603b8d3c1b32ea81e736640e0b2471f
Hendi Santika <hendisantika@gmail.com>
2019-01-19 05:59:56 +0700

    #9 - Modify Book Rest Controller

--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
@@ -2,11 +2,11 @@ package com.hendisantika.springbootrestapipostgresql.controller;
 
 import com.hendisantika.springbootrestapipostgresql.entity.Book;
 import com.hendisantika.springbootrestapipostgresql.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
 import java.util.Collection;
 
 /**
@@ -23,42 +23,30 @@ import java.util.Collection;
 @RequestMapping("/api/books")
 public class BookRestController {
 
+    @Autowired
     private BookRepository repository;
 
-    @Inject
-    public void setRepository(BookRepository repository) {
-        this.repository = repository;
-    }
-
-    @RequestMapping(
-            method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<?> addBook(@RequestBody Book book) {
         return new ResponseEntity<>(repository.save(book), HttpStatus.CREATED);
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<Collection<Book>> getAllBooks() {
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public ResponseEntity<Book> getBookWithId(@PathVariable Long id) {
         return new ResponseEntity<>(repository.findOne(id), HttpStatus.OK);
     }
 
-    @RequestMapping(
-            params = {"name"},
-            method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<Collection<Book>> findBookWithName(@RequestParam(value = "name") String name) {
         return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.PUT)
+    @PutMapping("/{id}")
     public ResponseEntity<Book> updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
 
         Book currentBook = repository.findOne(id);
@@ -69,15 +57,12 @@ public class BookRestController {
         return new ResponseEntity<>(repository.save(currentBook), HttpStatus.OK);
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public void deleteBookWithId(@PathVariable Long id) {
         repository.delete(id);
     }
 
-    @RequestMapping(
-            method = RequestMethod.DELETE)
+    @DeleteMapping
     public void deleteAllBooks() {
         repository.deleteAll();
     }
