72092780216d4880bc3d424b84c6a63dcc696c2f
Hendi Santika <hendisantika@gmail.com>
2019-01-18 22:03:05 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/SpringBootRestApiPostgresqlApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootrestapipostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiPostgresqlApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hendisantika/springbootrestapipostgresql/SpringBootRestApiPostgresqlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootrestapipostgresql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestApiPostgresqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
