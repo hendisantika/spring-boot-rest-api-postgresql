25518c5be13bded3f962d37bc5ef6802b833630c
Hendi Santika <hendi.santika@allianz.co.id>
2019-12-16 12:56:34 +0700

    #18 - Upgraded into Spring Boot 2.2.0.RELEASE version

--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
@@ -5,9 +5,18 @@ import com.hendisantika.springbootrestapipostgresql.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,7 +47,7 @@ public class BookRestController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookWithId(@PathVariable Long id) {
-        return new ResponseEntity<>(repository.findOne(id), HttpStatus.OK);
+        return new ResponseEntity<Book>(repository.findById(id).get(), HttpStatus.OK);
     }
 
     @GetMapping(params = {"name"})
@@ -49,7 +58,8 @@ public class BookRestController {
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
 
-        Book currentBook = repository.findOne(id);
+        Optional<Book> currentBookOpt = repository.findById(id);
+        Book currentBook = currentBookOpt.get();
         currentBook.setName(book.getName());
         currentBook.setDescription(book.getDescription());
         currentBook.setTags(book.getTags());
@@ -59,7 +69,7 @@ public class BookRestController {
 
     @DeleteMapping("/{id}")
     public void deleteBookWithId(@PathVariable Long id) {
-        repository.delete(id);
+        repository.deleteById(id);
     }
 
     @DeleteMapping
